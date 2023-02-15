8a8d66d7bc62997750bde6f32d1feb2834ec4535
Guillaume Longrais <guillaume.longrais@paf.com>
2023-02-15 17:22:23 +0100

    Created Stock Model

--- stocks/models.py
+++ stocks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+    symbol = models.CharField("Symbol", max_length=20, nullable=False, primary_key=True)
+    name = models.CharField("Name", max_length=240)
+    currentPrice = models.FloatField("CurrentPrice")
+    previousClose = models.FloatField("PreviousClose")
+    sector = models.CharField("Sector", max_length=50)
+    dividendYield = models.FloatField("DividendYield")
+    logoUrl = models.CharField("LogoURL", max_length=240)
+
+    def __str__(self):
+        return self.name
