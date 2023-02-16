2354decc1847a884f45ffd8b5fa802b04c77feb2
Guillaume Longrais <guillaume.longrais@paf.com>
2023-02-16 15:00:36 +0100

    Fixed secret path

--- mystocks_proj/mystocks_proj/settings.py
+++ mystocks_proj/mystocks_proj/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from SECRET.secret import DJANGO_SECRET
+from mystocks_proj.SECRET.secret import DJANGO_SECRET
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
