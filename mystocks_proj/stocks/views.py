6afdf2da102b8d6fd217f7919028ac70709a1483
Guillaume Longrais <guillaume.longrais@paf.com>
2023-02-16 13:33:27 +0100

    Created stock base API

--- mystocks_proj/stocks/views.py
+++ mystocks_proj/stocks/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from .models import Stock
+from .serializers import *
+
+@api_view(['GET', 'POST'])
+def stocks_list(request):
+    if request.method == 'GET':
+        data = Stock.objects.all()
+
+        serializer = StocksSerializer(data, context={'request': request}, many=True)
+
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = StocksSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def stocks_detail(request, pk):
+    try:
+        stock = Stock.objects.get(pk=pk)
+    except Stock.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = StocksSerializer(stock, data=request.data,context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        stock.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
