579592d6d8380e21ae3971293ad93023866b49b9
Guillaume Longrais <guillaume.longrais@orange.fr>
2023-02-20 19:21:10 +0100

    Added depth=1 to the serializer

--- mystocks_proj/stocks/serializers.py
+++ mystocks_proj/stocks/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
-from .models import Stock
+from .models import Stock, StockHolding
 
+# Define a serializer for the Stock model
 class StocksSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Stock 
-        fields = ('pk', 'symbol', 'name', 'currentPrice', 'previousClose', 'sector', 'dividendYield', 'logoUrl')
+        # Set the model that this serializer will be used to serialize
+        model = Stock
+        # Specify that all fields on the Stock model should be included in the serialized output
+        fields = '__all__'
+
+# Define a serializer for the StockHolding model
+class StockHoldingSerializer(serializers.ModelSerializer):
+
+    # Include a nested representation of the related Stock instance in the serialized output
+    stock = StocksSerializer(many=False, read_only=True)
+
+    class Meta:
+        # Set the model that this serializer will be used to serialize
+        model = StockHolding 
+        # Specify that all fields on the StockHolding model should be included in the serialized output
+        fields = '__all__'
+        # Specify that one level of related models should be included in the serialized output
+        depth=1
