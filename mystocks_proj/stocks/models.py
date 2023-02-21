ba5981ba0258a575d9da984524667d0f5cffef08
Guillaume Longrais <guillaume.longrais@orange.fr>
2023-02-21 17:39:10 +0100

    Fixed model types

--- mystocks_proj/stocks/models.py
+++ mystocks_proj/stocks/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Stock(models.Model):
     symbol = models.CharField("Symbol", max_length=20)
     name = models.CharField("Name", max_length=240)
-    currentPrice = models.FloatField("CurrentPrice")
-    previousClose = models.FloatField("PreviousClose")
+    currentPrice = models.DecimalField(max_digits=10, decimal_places=2)
+    previousClose = models.DecimalField(max_digits=10, decimal_places=2)
     sector = models.CharField("Sector", max_length=50)
-    dividendYield = models.FloatField("DividendYield")
+    dividendYield = models.DecimalField(max_digits=10, decimal_places=2)
     logoUrl = models.CharField("LogoURL", max_length=240)
 
     def __str__(self):
