3d3acff0771ed1c0794bfc528d97fdc9439f9ef9
Guillaume Longrais <guillaume.longrais@paf.com>
2023-02-24 17:34:10 +0100

    Created top bar

--- mystocks_fe/src/App.js
+++ mystocks_fe/src/App.js
@@ -1,13 +1,26 @@
-import React, { Component, Fragment } from "react";
+import React, { useState, Component, Fragment } from "react";
 import Header from "./scenes/global/Header";
 import Home from "./components/Home";
+import { ColorModeContext, useMode } from "./theme";
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
+
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <Fragment>
-      <Header />
-      <Home />
-    </Fragment>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Header setIsSidebar={setIsSidebar} /> 
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
